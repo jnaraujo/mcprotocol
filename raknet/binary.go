89cd15dce4351de4b4e3203b2da1735a47067d1b
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-29 13:07:10 -0300

    feat: Add Buffer struct and methods for reading and writing binary data

---
+++ raknet/binary.go
@@ -0,0 +1,154 @@
+package raknet
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+
+	"github.com/jnaraujo/mcprotocol/assert"
+)
+
+const (
+	segment_bits  = 0x7F // the value itself
+	continue_bits = 0x80 // if there is more bytes after current byte
+)
+
+var (
+	ErrTooBig = errors.New("too big")
+)
+
+type Buffer struct {
+	data *bytes.Buffer
+}
+
+func NewBuffer() *Buffer {
+	return &Buffer{
+		data: new(bytes.Buffer),
+	}
+}
+
+func NewBufferFrom(data *bytes.Buffer) *Buffer {
+	return &Buffer{
+		data: data,
+	}
+}
+
+func (buf *Buffer) WriteByte(value byte) error {
+	return buf.data.WriteByte(value)
+}
+
+func (buf *Buffer) ReadByte() (byte, error) {
+	currentByte, err := buf.data.ReadByte()
+	if err != nil {
+		return 0, err
+	}
+	return currentByte, nil
+}
+
+func (buf *Buffer) WriteVarInt(value int32) error {
+	assert.Assert(value >= 0, "value should the greater than 0")
+	for {
+		if (value & ^segment_bits) == 0 {
+			return buf.WriteByte(byte(value))
+		}
+		err := buf.WriteByte(byte((value & segment_bits) | continue_bits))
+		if err != nil {
+			return err
+		}
+		value >>= 7
+	}
+}
+
+func (buf *Buffer) ReadVarInt() (int32, error) {
+	val := int32(0)
+	pos := int32(0)
+	for {
+		currentByte, err := buf.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		val |= int32(currentByte&segment_bits) << pos
+		// if there is no byte after the current
+		if currentByte&continue_bits == 0 {
+			break
+		}
+		pos += 7
+		if pos >= 32 {
+			return 0, ErrTooBig
+		}
+	}
+	return val, nil
+}
+
+func (buf *Buffer) WriteString(str string) error {
+	err := buf.WriteVarInt(int32(len(str)))
+	if err != nil {
+		return err
+	}
+	_, err = buf.data.WriteString(str)
+	return err
+}
+
+func (buf *Buffer) ReadString() (string, error) {
+	length, err := buf.ReadVarInt()
+	if err != nil {
+		return "", err
+	}
+
+	strBytes := make([]byte, length)
+	_, err = buf.data.Read(strBytes)
+	if err != nil {
+		return "", err
+	}
+
+	return string(strBytes), nil
+}
+
+func (buf *Buffer) ReadUShort() (uint16, error) {
+	b := make([]byte, 2)
+	_, err := buf.data.Read(b)
+	if err != nil {
+		return 0, err
+	}
+	return binary.BigEndian.Uint16(b), nil
+}
+
+func (buf *Buffer) WriteUShort(value uint16) error {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, value)
+	_, err := buf.data.Write(b)
+	return err
+}
+
+func (buf *Buffer) ReadVarLong() (int64, error) {
+	val := int64(0)
+	pos := uint8(0)
+	for {
+		currentByte, err := buf.ReadByte()
+		if err != nil {
+			return 0, err
+		}
+		val |= int64(currentByte&segment_bits) << pos
+		if currentByte&continue_bits == 0 {
+			break
+		}
+		pos += 7
+		if pos >= 64 {
+			return 0, ErrTooBig
+		}
+	}
+	return val, nil
+}
+
+func (buf *Buffer) WriteVarLong(value int64) error {
+	for {
+		if value & ^segment_bits == 0 {
+			return buf.WriteByte(byte(value))
+		}
+		err := buf.WriteByte(byte((value & segment_bits) | continue_bits))
+		if err != nil {
+			return err
+		}
+		value >>= 7
+	}
+}
---
+++ raknet/binary_test.go
@@ -0,0 +1,122 @@
+package raknet
+
+import (
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWriteByte(t *testing.T) {
+	buf := NewBuffer()
+
+	buf.WriteByte(byte(123))
+	assert.Equal(t, byte(123), buf.data.Bytes()[0])
+
+	buf.WriteByte(byte(255))
+	assert.Equal(t, byte(255), buf.data.Bytes()[1])
+}
+
+func TestReadByte(t *testing.T) {
+	buf := NewBuffer()
+
+	buf.WriteByte(byte(123))
+
+	actual, err := buf.ReadByte()
+	assert.Nil(t, err)
+	assert.Equal(t, byte(123), actual)
+
+	buf.WriteByte(byte(255))
+	buf.WriteByte(byte(154))
+
+	actual, err = buf.ReadByte()
+	assert.Nil(t, err)
+	assert.Equal(t, byte(255), actual)
+
+	actual, err = buf.ReadByte()
+	assert.Nil(t, err)
+	assert.Equal(t, byte(154), actual)
+}
+
+func TestReadVarInt(t *testing.T) {
+	buf := NewBuffer()
+	buf.WriteByte(0x80)
+	buf.WriteByte(0x01)
+
+	actual, err := buf.ReadVarInt()
+	assert.Nil(t, err)
+	assert.Equal(t, int32(128), actual)
+}
+
+func TestWriteVarInt(t *testing.T) {
+	buf := NewBuffer()
+
+	buf.WriteVarInt(0)
+	buf.WriteVarInt(128)
+	buf.WriteVarInt(2147483647)
+
+	assert.Equal(t, []byte{0x00}, buf.data.Bytes()[0:1])
+	assert.Equal(t, []byte{0x80, 0x01}, buf.data.Bytes()[1:3])
+	assert.Equal(t, []byte{0xff, 0xff, 0xff, 0xff, 0x07}, buf.data.Bytes()[3:8])
+}
+
+func TestWriteString(t *testing.T) {
+	expected := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a elit ex."
+	buf := NewBuffer()
+	buf.WriteString(expected)
+	length, _ := buf.ReadVarInt()
+
+	assert.Equal(t, int32(75), length)
+	assert.Equal(t, expected, buf.data.String())
+}
+
+func TestReadString(t *testing.T) {
+	expected := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a elit ex."
+	buf := NewBuffer()
+	buf.WriteString(expected)
+
+	actual, err := buf.ReadString()
+	assert.Nil(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestReadUShort(t *testing.T) {
+	expected := make([]byte, 2)
+	binary.BigEndian.PutUint16(expected, 21312)
+
+	buf := NewBuffer()
+	buf.data.Write(expected)
+
+	value, err := buf.ReadUShort()
+	assert.Nil(t, err)
+	assert.Equal(t, uint16(21312), value)
+}
+
+func TestWriteUShort(t *testing.T) {
+	expected := make([]byte, 2)
+	binary.BigEndian.PutUint16(expected, 13456)
+
+	buf := NewBuffer()
+	buf.WriteUShort(13456)
+
+	assert.Equal(t, expected, buf.data.Bytes())
+}
+
+func TestReadVarLong(t *testing.T) {
+	buf := NewBuffer()
+	buf.data.Write([]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f})
+
+	actual, err := buf.ReadVarLong()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(9223372036854775807), actual)
+}
+
+func TestWriteVarLong(t *testing.T) {
+	buf := NewBuffer()
+
+	buf.WriteVarLong(0)
+	buf.WriteVarLong(9223372036854775807)
+
+	assert.Equal(t, []byte{0x00}, buf.data.Bytes()[0:1])
+	assert.Equal(t, []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f}, buf.data.Bytes()[1:10])
+}

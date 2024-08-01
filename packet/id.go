1804593f57841b52a3af5112e6c810eef41c162f
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-08-01 17:15:44 -0300

    feat: Add String() method to PacketID for string representation

--- packet/id.go
+++ packet/id.go
@@ -1,8 +1,14 @@
 package packet
 
+import "fmt"
+
 type PacketID byte
 
 const (
 	IDServerIdentification PacketID = 0
 	IDPing                 PacketID = 1
 )
+
+func (pID PacketID) String() string {
+	return fmt.Sprintf("0x%x", byte(pID))
+}
